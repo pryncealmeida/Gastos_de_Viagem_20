ba72b16a0ad4da23c69899ddb3ad3b51008a8775
Prynce <pryncealmeida@gmail.com>
2020-12-03 17:18:07 -0300

    Commit 1715 03122020

---
+++ app/src/main/java/com/example/gastosdeviagem/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gastosdeviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//A
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
