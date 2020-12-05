60e5ed0d45b5c14bdda38a3b7286f11c5d81e6b1
Prynce <pryncealmeida@gmail.com>
2020-12-05 12:14:42 -0300

    Commit 1214 05122020

--- app/src/main/java/com/example/gastosdeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastosdeviagem/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.gastosdeviagem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 //A
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+
+
+
+
     }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        if (id == R.id.buttonCalculate){
+            calculate()
+        }
+
+    }
+
+
+
+        private fun calculate() {
+
+        if(ValidationOK()){
+            try {
+                val distance = editText_distancia.text.toString().toFloat()
+                val autonomia = editText_autonomia.text.toString().toFloat()
+                val preco = editText_preco.text.toString().toFloat()
+
+                val resultado = (distance * preco) / autonomia
+                totalValue.text = "R$ ${"%.2f".format(resultado)}"
+            }
+            catch (nfe : NumberFormatException){
+                Toast.makeText(this, getString(R.string.Valores_validos), Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+        else{
+        Toast.makeText(this, getString(R.string.Preencha_campos), Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+
+
+    private fun ValidationOK(): Boolean{
+    return( editText_distancia.text.toString() != ""
+            && editText_autonomia.text.toString() != ""
+            && editText_autonomia.text.toString()!= "0"
+            && editText_preco.text.toString() != "")
+
+
+
+    }
+
+
 }
